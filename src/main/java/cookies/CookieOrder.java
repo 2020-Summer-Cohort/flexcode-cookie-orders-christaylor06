8e2c448ce4fbb44dedd5919107fa46c269644bf8
christaylor06 <christaylor06@yahoo.com>
2020-05-15 15:56:30 -0400

    Final submission cookie orders

--- src/main/java/cookies/CookieOrder.java
+++ src/main/java/cookies/CookieOrder.java
@@ -1,4 +1,21 @@
 package cookies;
-
+//managing two fields
 public class CookieOrder {
+    private int numBoxes;
+    private String variety;
+//constructor -setting fields up
+    public CookieOrder(String localVariety, int localNumBoxes) {
+        this.numBoxes = localNumBoxes;
+        this.variety = localVariety;
+    }
+//access or method 1
+    public int getNumberOfBoxes() {
+
+        return this.numBoxes;
+    }
+//accessor method 2
+    public String getVariety() {
+
+        return this.variety;
+    }
 }
